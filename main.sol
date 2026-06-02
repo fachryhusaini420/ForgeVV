af8a572bafb82a0a713f81ece5f3a19a69e0cbbf
fachryhusaini420 <fachryhusaini420@gmail.com>
2026-06-02 04:31:58 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -1213,3 +1213,84 @@ contract ForgeVV {
             rewards[i] = p.rewardAccruedWei;
         }
     }
+
+    function batchFrame_23(address saver, uint256 start, uint256 count)
+        external
+        view
+        returns (uint256[] memory principals, uint256[] memory rewards)
+    {
+        uint256 n = podCountOf[saver];
+        if (start >= n) {
+            return (new uint256[](0), new uint256[](0));
+        }
+        uint256 end = start + count;
+        if (end > n) end = n;
+        uint256 len = end - start;
+        principals = new uint256[](len);
+        rewards = new uint256[](len);
+        for (uint256 i; i < len; ++i) {
+            SavingsPod storage p = podsOf[saver][start + i];
+            principals[i] = p.principalWei;
+            rewards[i] = p.rewardAccruedWei;
+        }
+    }
+
+    function batchFrame_24(address saver, uint256 start, uint256 count)
+        external
+        view
+        returns (uint256[] memory principals, uint256[] memory rewards)
+    {
+        uint256 n = podCountOf[saver];
+        if (start >= n) {
+            return (new uint256[](0), new uint256[](0));
+        }
+        uint256 end = start + count;
+        if (end > n) end = n;
+        uint256 len = end - start;
+        principals = new uint256[](len);
+        rewards = new uint256[](len);
+        for (uint256 i; i < len; ++i) {
+            SavingsPod storage p = podsOf[saver][start + i];
+            principals[i] = p.principalWei;
+            rewards[i] = p.rewardAccruedWei;
+        }
+    }
+
+    function batchFrame_25(address saver, uint256 start, uint256 count)
+        external
+        view
+        returns (uint256[] memory principals, uint256[] memory rewards)
+    {
+        uint256 n = podCountOf[saver];
+        if (start >= n) {
+            return (new uint256[](0), new uint256[](0));
+        }
+        uint256 end = start + count;
+        if (end > n) end = n;
+        uint256 len = end - start;
+        principals = new uint256[](len);
+        rewards = new uint256[](len);
+        for (uint256 i; i < len; ++i) {
+            SavingsPod storage p = podsOf[saver][start + i];
+            principals[i] = p.principalWei;
+            rewards[i] = p.rewardAccruedWei;
+        }
+    }
+
+    function batchFrame_26(address saver, uint256 start, uint256 count)
+        external
+        view
+        returns (uint256[] memory principals, uint256[] memory rewards)
+    {
+        uint256 n = podCountOf[saver];
+        if (start >= n) {
+            return (new uint256[](0), new uint256[](0));
+        }
+        uint256 end = start + count;
+        if (end > n) end = n;
+        uint256 len = end - start;
+        principals = new uint256[](len);
+        rewards = new uint256[](len);
+        for (uint256 i; i < len; ++i) {
+            SavingsPod storage p = podsOf[saver][start + i];
+            principals[i] = p.principalWei;
